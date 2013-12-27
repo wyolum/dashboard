e530ffdab0e90cf4fa067de2acadbc68daf6df61
Justin Shaw <wyojustin@gmail.com>
2013-12-27 19:09:17 +0000

    edge now computes up to 10 deltas

--- edge.py
+++ edge.py
@@ -1,17 +1,34 @@
 import Adafruit_BBIO.GPIO as GPIO
+import time
+from numpy import diff
 
 GPIO.setup("P8_9", GPIO.IN)
 GPIO.setup("P8_10", GPIO.IN)
 
+class Buff:
+    def __init__(self, max_size=10):
+        self.data = []
+        self.max_size = max_size
+
+    def append(self, item):
+        self.data.append(item)
+	self.data = self.data[-self.max_size:]
+
+    def get(self):
+        return self.data[:]
+
+times = {'P8_9':Buff(),
+         'P8_10':Buff()}
+
 def cadence_cb(pin_id):
-    print pin_id, 
-    if GPIO.input(pin_id):
-        print("HIGH")
-    else:
-        print("LOW")
-
+    times[pin_id].append(time.time())
+    
 GPIO.cleanup()
-GPIO.add_event_detect("P8_9", GPIO.BOTH, cadence_cb)
-GPIO.add_event_detect("P8_10", GPIO.BOTH, cadence_cb)
+for key in times:
+    GPIO.add_event_detect(key, GPIO.FALLING, cadence_cb, 0)
 
-raw_input("<Return> to stop\n")
+while True:
+    for key in times:
+        print key, diff(times[key].get())
+    time.sleep(1)
+
